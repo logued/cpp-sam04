3a512e0f85a6c0a2d5b176a40012d0526d40c57b
dermo <dermot.logue@dkit.ie>
2022-02-17 10:48:49 +0000

    Update to comments and TODO exercises

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-// Pointer_Basics.cpp	Feb 2022
+// Pointer Basics	Feb 2022
 
 #include <iostream>
 using namespace std;
 
 void demo1();   // function prototypes
 void demo2();
+void swap_by_pointer(int* , int*);
 
 int main()
 {
@@ -22,14 +23,14 @@ void demo1()		// Pointer basics - declaring a pointer, initialising a pointer an
     int* p = nullptr;
     // declares variable p as type "pointer to int" (or 'p is a pointer to an int').
     // p is a variable that can store the address of an int.
-    // It is good practice to assign a 'nullptr' value to the pointer initially.
+    // It is good practise to assign a 'nullptr' value to the pointer initially.
     // nullptr is a special value indicating that the pointer doesn't
     // point at any memory location.
 
     p = &x;
     // '&x' means: get the memory address of the variable x. ('&' is the 'address-of' operator)
-    // An address is itself an integer value.
-    // We can assign the address of x into p, as a pointer variable can store an address.
+    // An address is itself an integer value (normally 8-bytes long)
+    // We can assign the address of x into pointer variable p, as a pointer variable can store an address.
 
     cout << "x: (value in x): " << x << endl;		//value of x
     cout << "&x: (address of x): " << &x << endl;	//address of x
@@ -42,7 +43,7 @@ void demo1()		// Pointer basics - declaring a pointer, initialising a pointer an
     cout << "*p (de-referencing the pointer)(i.e. get value at location where p points to):\t";
     cout << *p << endl;
 
-    // note that, here, the type of *p is the same as the type that
+    // note that the type of *p is the same as the type that
     // the pointer p was declared to point at.
     // p was declared to be a pointer to an int, therefore,
     // *p is of type int (*p is the thing that p points to)
@@ -53,39 +54,37 @@ void demo1()		// Pointer basics - declaring a pointer, initialising a pointer an
 
     *p = 99;
     // put the value 99 into where p is pointing.
-    // p is pointing at x, so 99 will be put into the variable x
+    // p is pointing at x, so *p dereferences p, giving access to the memory space that p points to,
+    // 99 will be put into that memory space (which is into variable x)
     // The value in x has been changed by use of the pointer.
 
     cout << "... after executing *p = 99;";
-    cout << "*p: " << *p << endl;
+    cout << "*p: " << *p << endl;.
     cout << "x: (value in x): " << x << endl;	//value of x was changed using pointer p
 
-    /*TODO in class -- complete in class and confirm your output.
-     * Q1.
-    - decare a variable of type double called 'height' and assign it your height in meters (e.g. 1.56)
-    - declare a variable pHeight of type 'pointer to double';
-    - get the address of the height variable and assign it to the pointer pHeight;
-    - print out the height variable
-    - print out the address of the height variable using '&'
-    - print out the value of the pointer pHeight
-    - print out the value of the height variable by de-referencing the pHeight pointer variable
-    - assign a new value to the height variable e.g. 1.88
-    - print out the value
-    - assign a new value the height variable using the pointer pHeight  (dereference as an l-value)
-    - DRAW a memory diagram on paper to show the state of all variables just before end of main()
+    //TODO in class -- complete in class and confirm your output
+    // Q1.
+    // - declare a variable of type double called 'height' and assign it your height in meters (e.g. 1.56)
+    //    - declare a variable pHeight of type 'pointer to double';
+    //    - get the address of the height variable and assign it to the pointer pHeight;
+    //    - print out the height variable
+    //    - print out the address of the height variable using '&'
+    //    - print out the value of the pointer pHeight
+    //    - print out the value of the height variable by de-referencing the pHeight pointer variable
+    //    - assign a new value to the height variable e.g. 1.88
+    //    - print out the value
+    //    - assign a new value the height variable using the pointer pHeight  (dereference as an l-value)
+    //    - DRAW a memory diagram on paper to show the state of all variables just before end of main()
 
-    Q.2 Declare and initialize a double d with the value 3.14.
-    Declare a pointer pd and set it to point at the variable d.
-    Using the pointer, set the value of d to 3.14159, and output
-    the new value of d using the pointer.
+    //TODO Q.2 Declare and initialize a double d with the value 3.14.
+    //    Declare a pointer pd and set it to point at the variable d.
+    //    Using the pointer, set the value of d to 3.14159, and output
+    //    the new value of d using the pointer.
 
-    Q.3 Declare a pointer to a char called pc, but don't assign it a value.
-    Print out the value of pc.  Look at the value. What does the value represent?
-
-    Output *pc (that is, the thing that pc is pointing at)
-     -- did it run?, if so, study the output. What has happened?  Explain!
-
-    */
+    //TODO Q.3 Declare a pointer to a char called pc, but don't assign it a value.
+    // Print out the value of pc.  Look at the value. What does the value represent?
+    //    Output *pc (that is, the thing that pc is pointing at)
+    // -- did it run?, if so, study the output. What has happened?  Explain!
 }
 
 // demo2()
@@ -120,14 +119,14 @@ void demo2()	// swap values of two variables using by passing their addresses
 
     cout << "After swap_by_pointer(): x=" << x << ",  y=" << y << endl;
 
-    /*TODO  Code a solution to the question below and test.
-    Q4.
-    - write a function called add() with parameters to accept the addresses
-      of two int arguments.  The function should add the values of the objects
-      pointed to, and return an int.  Test your program.
-
-    - how would you prevent the pointer parameters in add() from modifying the
-      objects that they point to.  Try it out.
-
-      */
+    //TODO  Code a solution to the question below and test.
+    // Q4.
+    //    - write a function called add() with two pointer parameters to accept the addresses
+    //      of two int arguments.  The function should add the values of the objects
+    //      pointed to, and return an int.  Test your program.
+    //
+    //    - how would you prevent the pointer parameters in add() from modifying the
+    //      objects that they point to.  Try it out.
+    //
+    //
 }
